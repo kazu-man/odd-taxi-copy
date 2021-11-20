8a33821b0c72552ff63579dc37ddb6fa1a8921f5
ユーザ <0ht53152387219x@mail.com>
2021-11-20 18:07:51 +0900

    CastStaffを追加

--- components/MainContents.vue
+++ components/MainContents.vue
@@ -20,8 +20,13 @@
                 <Character />
             </div>
 
-            <div class="black-bg" style="height:1000px" />
+            <div class="black-bg" >
+                <CastStaff />
+            </div>
+
+
             <div style="height:1000px" />
+            <div class="black-bg" style="height:1000px" />
             <div class="black-bg" style="height:1000px" />
 
         </div>
