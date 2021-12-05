75e680182daad5a5f0d20aa1740b91b1b8105d69
ユーザ <0ht53152387219x@mail.com>
2021-12-05 18:19:08 +0900

    characterの詳細を追加

--- components/common-parts-components/modal-contents/CharacterDetail.vue
+++ components/common-parts-components/modal-contents/CharacterDetail.vue
@@ -1,15 +1,27 @@
 <template>
     <div class="character-detail">
-        <v-row>
-            <v-col>
+        <v-row class="character-row">
+            <v-col cols="12" sm="4" style="text-align: center;">
                 <img :src="'/images/' + modalInfo.character.characterImg" />
             </v-col>
             <v-col>
-                {{modalInfo.character.characterName}}
-                {{modalInfo.character.characterVoice}}
-                {{modalInfo.character.characterImg}}
-                {{modalInfo.character.characterInfo}}
-                {{modalInfo.character.characterDetail}}
+
+                <div class="character-title">
+                    {{modalInfo.character.characterName}}
+                    <span>
+                        {{modalInfo.character.characterVoice}}
+                    </span>
+
+                </div>
+                <div class="character-info">
+                    {{modalInfo.character.characterInfo}}
+
+                </div>
+                <div class="character-info-detail">
+                    {{modalInfo.character.characterDetail}}
+
+                </div>
+
             </v-col>
         </v-row>
     </div>
@@ -29,12 +41,41 @@
 
 <style scoped>
 img{
-    height:200px;
+    max-height: 300px;
 }
 .character-detail{
     height:100%;
-            display: flex;
-        justify-content: center;
-        align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
+
+.character-row{
+    justify-content: center;
+    align-items: center;
+}
+
+.character-title{
+    font-size: 40px;
+    font-weight: 900;
+    border-bottom: 1px rgb(244, 95, 74) solid;
+    margin-bottom:20px;
+}
+.character-title span{
+    font-size: 20px;
+    font-weight: normal;
+}
+.character-info{
+    background: rgb(244, 95, 74);
+    color: rgb(30, 91, 85);
+    font-size: 15px;
+    padding: 3px;
+    text-align: center;
+    margin-bottom:20px;
+    max-width:250px;
+}
+.character-info-detail{
+    
+}
+
 </style>
